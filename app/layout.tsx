ba84186533cc43f5479a1c567602b8f6be82bbc3
THSK4U <157622490+THSK4U@users.noreply.github.com>
2025-10-22 20:31:45 +0100

    Update app metadata for title and description

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,9 +9,9 @@ const _geist = Geist({ subsets: ["latin"] })
 const _geistMono = Geist_Mono({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "v0 App",
-  description: "Created with v0",
-  generator: "v0.app",
+  title: "main tester App",
+  description: "Created with THSK4U",
+  generator: "sellak.vercel.app",
 }
 
 export default function RootLayout({
